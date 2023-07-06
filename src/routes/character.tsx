18f855b9c5b8f7f78f68c580d5351762991d3052
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 02:06:35 +0300

    use SkeletonLoading with SkeletonProps

--- src/routes/character.tsx
+++ src/routes/character.tsx
@@ -1,13 +1,23 @@
 import { PATH } from "../constants/paths";
 import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
+import { SkeletonLoading } from "../components/SkeletonLoading";
+import { SkeletonProps } from "react-loading-skeleton";
 
 export default function CharacterRoutes() {
+    const skeletonProps: SkeletonProps = {
+        count: 10,
+        height: 50,
+        width: 50,
+        duration: 2,
+        circle: true
+    }
+
     return (
         <Routes>
             <Route path={PATH.CHARACTERS}
                 Component={() => (
-                    <Suspense fallback={ <div>Loading...</div> }>
+                    <Suspense fallback={<SkeletonLoading {...skeletonProps} />}>
                         <div>Characters</div>
                     </Suspense>
                 )} />
