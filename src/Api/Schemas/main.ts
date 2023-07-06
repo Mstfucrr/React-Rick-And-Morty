07e872bc67272527d2e32c7274776cf6dc8840b6
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 02:42:17 +0300

    add main interface

---
+++ src/Api/Schemas/main.ts
@@ -0,0 +1,14 @@
+
+export interface main {
+    id: string;
+    name: string;
+    created: string;
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next: number | null;
+    prev: number | null;
+}
+
