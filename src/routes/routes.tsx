b9c0387625f2cef9b06054b9c2ff7800e42fae9e
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 02:22:24 +0300

    use LocationRoutes in main Routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import CharacterRoutes from "./character";
+import LocationRoutes from "./location";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <CharacterRoutes />
-            
+            <LocationRoutes />
         </BrowserRouter>
     )
 }
