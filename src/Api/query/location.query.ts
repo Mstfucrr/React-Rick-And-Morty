75cdf5e9726f309a1537fe6a6fb60dff3c96c2ef
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 16:02:04 +0300

    add Location Queries

---
+++ src/Api/query/location.query.ts
@@ -0,0 +1,49 @@
+import { FilterLocation } from "../Schemas/location";
+import { gql } from "graphql-request";
+
+export function LocationByIdQuery() {
+    var query = gql`
+    query ($id: ID!) {
+        location(id: $id) {
+            id
+            name
+            type
+            dimension
+            residents {
+                id
+                name
+                image
+            }
+        }
+    }
+    `;
+    return query;
+}
+
+
+export function LocationsQuery() {
+    var query = gql`
+    query ($page: Int!, $filter: FilterLocation) {
+        locations(page: $page, filter: $filter) {
+            info {
+                count
+                pages
+                next
+                prev
+            }
+            results {
+                id
+                name
+                type
+                dimension
+                residents {
+                    id
+                    name
+                    image
+                }
+            }
+        }
+    }
+    `;
+    return query;
+}
