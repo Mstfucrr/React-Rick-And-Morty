c48437a8d3be42cef3cf0c03d7a07e59e65ee96f
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 20:36:30 +0300

    fix CharacterByIdQuery

--- src/Api/query/character.query.ts
+++ src/Api/query/character.query.ts
@@ -2,7 +2,7 @@ import { gql } from "graphql-request";
 
 export function CharacterByIdQuery() {
     const query = gql`
-    query {
+    query($id: ID!)  {
         character(id: $id) {
             id
             name
