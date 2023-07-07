e0061f451915d8ea3d175d6a623c7d785ecda227
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 20:43:01 +0300

    add status colors for styles

---
+++ src/assets/Colors/Colors.js
@@ -0,0 +1,30 @@
+
+export const colors = {
+    status: {
+        Alive: {
+            color: 'rgba(152, 205, 77, 0.05)',
+            hoverColor: 'rgba(152, 205, 77, 0.2)',
+            activeColor: "rgba(152, 205, 77, 0.5)",
+            border: "1px solid rgba(152, 205, 77, 1)"
+        },
+        Dead: {
+            color: 'rgba(185, 0, 0, 0.05)',
+            hoverColor: 'rgba(185, 0, 0, 0.2)',
+            activeColor: "rgba(185, 0, 0, 0.5)",
+            border: "1px solid rgba(185, 0, 0, 1)"
+        },
+        Unknown: {
+            color: 'rgba(184, 184, 184, 0.05)',
+            hoverColor: 'rgba(184, 184, 184, 0.2)',
+            activeColor: "rgba(184, 184, 184, 0.5)",
+            border: "1px solid rgba(184, 184, 184, 1)"
+        },
+        All: {
+            color: 'rgba(0, 0, 0, 0.05)',
+            border: '1px solid rgba(0, 0, 0, 1)',
+            hoverColor: 'rgba(0, 0, 0, 0.2)',
+            activeColor: 'rgba(0, 0, 0, 0.5)'
+
+        }
+    },
+}
