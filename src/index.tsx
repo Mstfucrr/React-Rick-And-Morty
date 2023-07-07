3402741df4dc50ec4a613a905d8a0350429eca52
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 16:01:30 +0300

    add and use index scss for fonts and bootstrap libs

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import './assets/scss/index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
