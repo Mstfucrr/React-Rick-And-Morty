ead6ab7f241e6951f16545af298750f516a50a72
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 20:42:31 +0300

    use navbar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "../components/navbar/navbar";
 import CharacterRoutes from "./character";
 import LocationRoutes from "./location";
 
 export default function Routes() {
     return (
         <BrowserRouter>
+            <Navbar />
             <CharacterRoutes />
             <LocationRoutes />
         </BrowserRouter>
