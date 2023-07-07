715809280a5cc91db520c30cba6f042726327baf
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-07 16:09:03 +0300

    add character queries

---
+++ src/Api/query/character.query.ts
@@ -0,0 +1,113 @@
+import { gql } from "graphql-request";
+
+export function CharacterByIdQuery() {
+    const query = gql`
+    query {
+        character(id: $id) {
+            id
+            name
+            status
+            species
+            type
+            gender
+            origin {
+                id
+                name
+                type
+                dimension
+            }
+            location {
+                id
+                name
+                type
+                dimension
+                residents {
+                    id
+                    name
+                    status
+                    species
+                    type
+                
+                }
+            }
+            image
+            episode {
+                id
+                name
+                air_date
+                episode
+                characters {
+                    id
+                    name
+                    status
+                    species
+                    type
+                }
+            }
+        }
+    }
+    `;
+    return query;
+}
+
+export function CharactersQuery() {
+    const query = gql`
+    query {
+        characters(page: $page, filter: $filter) {
+            info {
+                count
+                pages
+                next
+                prev
+                }
+            results {
+                name
+                status
+                species
+                type
+                gender
+                origin {
+                    id
+                }
+                location {
+                    id
+                    name
+                    type
+                    dimension
+                    residents {
+                        id
+                        name
+                        status
+                        species
+                        type
+                        gender
+                        image
+                    }
+                }
+                image
+                episode {
+                    id
+                    name
+                    air_date
+                    episode
+                    characters {
+                        id
+                        name
+                        status
+                        species
+                        type
+                        gender
+                        image
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+    `;
+    return query;
+}
+
