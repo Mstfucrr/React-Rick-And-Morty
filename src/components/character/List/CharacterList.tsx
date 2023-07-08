37def0126aed1604ce57180508094a617e6a5826
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-08 04:48:19 +0300

    use SkeletonLoading when viewing locations and character list

--- src/components/character/List/CharacterList.tsx
+++ src/components/character/List/CharacterList.tsx
@@ -1,18 +1,46 @@
 
+import { useEffect, useState } from 'react';
 import { Character } from '../../../Api/Schemas/character';
+import { SkeletonLoading } from '../../SkeletonLoading';
 import { CharacterElement } from './CharacterElement';
 import { CharacterListStyle } from './CharacterList.styles';
 
 export const CharacterList = (props: { characters: Character[] }) => {
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        setLoading(props.characters.length === 0);
+    }, [props.characters]);
+
+
+
+
     return (
         <CharacterListStyle>
 
             <div className="d-flex flex-wrap justify-content-center container-fluid">
-                {props.characters.map((character) => {
-                    return (
-                        <CharacterElement character={character} key={character.id} />
-                    );
-                })}
+
+                {loading ? (
+                    <>
+                        {Array.from(Array(20).keys()).map((_, index) => (
+                            <SkeletonLoading width={310} height={340}
+                                style={{
+                                    borderRadius: '15px'
+                                }}
+                                className="p-4 m-2"
+                            />
+                        ))}
+                    </>
+                ) :
+                    (
+                        props.characters.map((character) => {
+                            return (
+                                <CharacterElement character={character} key={character.id} />
+                            );
+                        })
+                    )
+                }
             </div>
         </CharacterListStyle>
     );
--- src/components/location/LocationList.tsx
+++ src/components/location/LocationList.tsx
@@ -5,18 +5,41 @@ import { LocationListStyle } from './LocationList.styles';
 
 import { Location } from '../../Api/Schemas/location';
 import { LocationElement } from './LocationElement';
+import { SkeletonLoading } from '../SkeletonLoading';
+import { useEffect, useState } from 'react';
 
 export const LocationList = (props: { locations: Location[] }) => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(props.locations.length === 0);
+    }, [props.locations]);
+
+
     return (
         <LocationListStyle>
             <div className='d-flex flex-wrap justify-content-center'>
-                {
-                    props.locations.map((location) => {
-                        return (
-                            <LocationElement location={location} key={location.id} />
-                        )
-                    })
-                }
+                {loading ? (
+                    <>
+                        {Array.from(Array(20).keys()).map((_, index) => (
+                            <SkeletonLoading
+                                width={440}
+                                height={140}
+                                style={{
+                                    borderRadius: '15px'
+                                }}
+                                className="p-4 m-2"
+                            />
+
+                        ))}
+                    </>
+                ) : (
+                    props.locations.map((location) => (
+
+                        <LocationElement location={location} key={location.id} />
+                    ))
+                )}
             </div>
         </LocationListStyle>
     )
