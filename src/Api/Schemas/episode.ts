507f98569627dbd4f0c7db4e7d679ff5b989aa9c
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-08 16:05:51 +0300

    add episonde query and schemas

--- src/Api/Schemas/episode.ts
+++ src/Api/Schemas/episode.ts
@@ -1,9 +1,23 @@
 import { Character } from "./character";
-import { main } from "./main";
+import { Info, main } from "./main";
 
 export interface Episode extends main {
-    air_date: string;
-    episode: string;
-    characters: Character[];
-    created: string;
-  }
+  air_date: string;
+  episode: string;
+  characters: Character[];
+  created: string;
+}
+
+export interface EpisodeData {
+  episode: Episode;
+}
+
+export interface EpisodesData {
+  info: Info;
+  results: Episode[];
+}
+
+export interface FilterEpisode {
+  name?: string;
+  episode?: string;
+}
