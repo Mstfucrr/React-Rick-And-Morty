393aa4ea18d5f7b1030b1a81df6e304f9eaaa8e2
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-08 04:44:47 +0300

    delete SkeletonLoading from routes

--- src/routes/character.tsx
+++ src/routes/character.tsx
@@ -1,35 +1,20 @@
 import { PATH } from "../constants/paths";
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-import { SkeletonLoading } from "../components/SkeletonLoading";
-import { SkeletonProps } from "react-loading-skeleton";
 import { CharactersPage } from "../pages/characters";
 import { CharacterDetailPage } from "../pages/characterDetail";
 
 export default function CharacterRoutes() {
-    const skeletonProps: SkeletonProps = {
-        count: 10,
-        height: 50,
-        width: 50,
-        duration: 2,
-        circle: true
-    }
-
     return (
         <Routes>
             <Route path={PATH.CHARACTERS}
                 Component={() => (
-                    <Suspense fallback={<SkeletonLoading {...skeletonProps} />}>
-                        <CharactersPage />
-                    </Suspense>
+                    <CharactersPage />
                 )} />
             <Route path={PATH.CHRACTER_DETAIL}
                 Component={() => (
-                    <Suspense fallback={<SkeletonLoading {...skeletonProps} />}>
-                        <CharacterDetailPage />
-                    </Suspense>
+                    <CharacterDetailPage />
                 )} />
-                                
+
         </Routes>
     )
 }
--- src/routes/location.tsx
+++ src/routes/location.tsx
@@ -1,32 +1,17 @@
 import { PATH } from "../constants/paths";
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-import { SkeletonLoading } from "../components/SkeletonLoading";
-import { SkeletonProps } from "react-loading-skeleton";
 import { LocationsPage } from "../pages/location";
 
 export default function LocationRoutes() {
-    const skeletonProps: SkeletonProps = {
-        count: 10,
-        height: 50,
-        width: 50,
-        duration: 2,
-        circle: true
-    }
-
     return (
         <Routes>
             <Route path={PATH.LOCATIONS}
                 Component={() => (
-                    <Suspense fallback={<SkeletonLoading {...skeletonProps} />}>
-                        <LocationsPage />
-                    </Suspense>
+                    <LocationsPage />
                 )} />
             <Route path={PATH.HOME}
                 Component={() => (
-                    <Suspense fallback={<SkeletonLoading {...skeletonProps} />}>
-                        <LocationsPage />
-                    </Suspense>
+                    <LocationsPage />
                 )} />
 
 
