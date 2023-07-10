32a2f16e64012d1a540b0f79c7e5086eb8c126ea
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-10 05:16:53 +0300

    Use framer-motion animation when listing lists

--- src/components/character/Detail/OtherCharacters.tsx
+++ src/components/character/Detail/OtherCharacters.tsx
@@ -1,6 +1,7 @@
 import { Character } from '../../../Api/Schemas/character';
 import { OtherCharacterItemView } from "./OtherCharacterItem";
 import { OtherCharactersTitle } from "./OtherCharacters.styles"
+import { motion } from "framer-motion"
 
 
 export const OtherCharactersView = (
@@ -8,19 +9,56 @@ export const OtherCharactersView = (
 ) => {
     return (
         <div className="d-flex flex-column">
-            <OtherCharactersTitle className="row">
-                <div className="col-12">
-                    Other characters
-                </div>
-            </OtherCharactersTitle>
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    x: -100,
+                }}
+                animate={{
+                    opacity: 1,
+                    x: 0,
+                }}
+                transition={{ duration: 0.7 }}
+                className="row"
+            >
+
+                <OtherCharactersTitle className="row">
+                    <div className="col-12">
+                        Other characters
+                    </div>
+                </OtherCharactersTitle>
+            </motion.div>
             <div>
                 <div className="d-flex flex-wrap">
-                    {props.otherCharacters.map((character) => {
+                    {props.otherCharacters.map((character, index) => {
                         return (
-                            <OtherCharacterItemView character={character} key={character.id} />
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                    y: -50,
+
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                    x: 0,
+
+                                }}
+                                transition={{ duration: 0.7, delay: index * 0.1 }}
+                                key={character.id}
+                                className="m-2"
+                                whileHover={{
+                                    scale: 1.02,
+                                    boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)',
+                                    transition: { duration: 0.1 }
+                                }}
+
+                            >
+                                <OtherCharacterItemView character={character} key={character.id} />
+                            </motion.div>
                         );
                     })}
-                    
+
                 </div>
             </div>
         </div>
--- src/components/location/LocationList.tsx
+++ src/components/location/LocationList.tsx
@@ -1,4 +1,5 @@
 import { LocationListStyle } from './LocationList.styles';
+import { motion } from "framer-motion"
 
 
 // companent
@@ -63,9 +64,39 @@ export const LocationList = (props: { locations: Location[] }) => {
                             ))}
                         </>
                     ) : (
-                        props.locations.map((location) => (
 
-                            <LocationElement location={location} key={location.id} />
+                        props.locations.map((location, index) => (
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                    scale: 0.5,
+                                    y: -100,
+                                    x: -100,
+                                    borderRadius: '15px',
+
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    scale: 1,
+                                    y: 0,
+                                    x: 0,
+                                    borderRadius: '15px',
+                                }}
+                                transition={{ duration: 0.5, delay: 0.08 * index }}
+                                key={location.id}
+                                className="m-2 ml-4 d-flex justify-content-center"
+                                whileHover={{
+                                    scale: 1.03,
+                                    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
+                                    transition: { duration: 0.2 }
+                                }}
+                                style={{
+                                    borderRadius: '15px',
+                                    width : '440px'
+                                }}
+                            >
+                                <LocationElement location={location} key={location.id} />
+                            </motion.div>
                         ))
                     )}
                 </div>
